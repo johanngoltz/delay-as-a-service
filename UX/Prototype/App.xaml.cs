1d55c3c31b337d2ff78a490487a423d6b7a65bcb
Johann Goltz <johann-goltz@hotmail.de>
2017-04-04 15:55:35 +0200

    Telemetrie, Icon

--- UX/Prototype/App.xaml.cs
+++ UX/Prototype/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.HockeyApp;
 
 namespace Prototype
 {
@@ -30,6 +31,7 @@ namespace Prototype
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            Microsoft.HockeyApp.HockeyClient.Current.Configure("03150b73f51e4ab3b0f48881ac1cac9b");
         }
 
         /// <summary>
